3a3cbb3545451340d4d32fa20c7214887719df10
7021307abekouta <s7021307@s.akita-u.ac.jp>
2024-01-26 12:09:12 +0900

    finish my class 7021307

--- Scoreboards.java
+++ Scoreboards.java
@@ -3,19 +3,24 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
 
+// 担当者　7021307 阿部紘大(Scoreboardsクラス、要件定義・パワポなどの資料作成)
 public class Scoreboards {
-    
+
+    // DBHelperクラスを使用してcsvファイルからスコアを読み込む
     private DBHelper dbHelper = new DBHelper("database.csv");
     private Scanner scanner;
 
+    //コンストラクタ
     public Scoreboards(Scanner scanner) {
         this.scanner = scanner;
     }
     
+    // スコアボードを表示する
     public void displayScoreBoard() {
+        // csvファイルからスコアの読み込み
         List<DBHelper.ScoreRecord> scores = dbHelper.readScores();
     
-        // スコアの高い順にソート
+        // スコアの高い順（降順）にソート
         Collections.sort(scores, Comparator.comparingInt((DBHelper.ScoreRecord s) -> s.score).reversed());
     
         // スコアを表形式で表示
@@ -23,7 +28,9 @@ public class Scoreboards {
         System.out.printf("%-7s %-25s %-15s %-10s\n", "順位", "プレイヤー名", "スコア", "日付");
         System.out.println("-----------------------------------------------------------------------------");
         int rank = 1;
+        // スコアをforループを使って表示
         for (DBHelper.ScoreRecord score : scores) {
+            // 未記入の場合の処理
             String playerName = score.playerName.isEmpty() ? "（未記入）" : score.playerName;
             System.out.printf("%-7d\t %-25s\t %-15d\t %-10s\n", rank, playerName, score.score, score.date);
             rank++;
